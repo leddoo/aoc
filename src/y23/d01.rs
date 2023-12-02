595082c9766daf90ed6d6e70e178fa1c2b4f2387
leddoo <leo@leddoo.net>
2023-12-02 08:50:26 +0100

    speed

--- src/y23/d01.rs
+++ src/y23/d01.rs
@@ -94,8 +94,10 @@ fn part_2(input: &str) -> i32 {
 }
 
 
-fn part_2_make_fsm() -> Vec<(u8, u8)> {
-    let words = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+fn part_2_make_fsm_ex(rev: bool, eol: bool) -> Vec<(u8, u8)> {
+    let words =
+        if !rev { [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ] }
+        else    { [ "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin" ] };
 
     let mut vocab = HashSet::new();
     for word in words {
@@ -146,7 +148,9 @@ fn part_2_make_fsm() -> Vec<(u8, u8)> {
         map[b'8' as usize] = (0, 8);
         map[b'9' as usize] = (0, 9);
 
-        map[b'\n' as usize] = (0, 255);
+        if eol {
+            map[b'\n' as usize] = (0, 255);
+        }
 
         let mut buf = Vec::from(state.as_bytes());
 
@@ -183,6 +187,10 @@ fn part_2_make_fsm() -> Vec<(u8, u8)> {
     }
 
     return table;
+}
+
+fn part_2_make_fsm() -> Vec<(u8, u8)> {
+    part_2_make_fsm_ex(false, true)
 }
 
 fn part_2_fsm(input: &str, table: &[(u8, u8)]) -> i32 {
@@ -251,6 +259,186 @@ fn part_2_fsm_threaded(input: &str, table: &[(u8, u8)]) -> i32 {
 }
 
 
+fn part_2_make_fsm2() -> (Vec<(u8, u8)>, Vec<(u8, u8)>) {
+    (part_2_make_fsm_ex(false, false),
+     part_2_make_fsm_ex(true,  false))
+}
+
+fn part_2_fsm2(input: &str, fwd: &[(u8, u8)], bwd: &[(u8, u8)]) -> i32 {
+    let mut result = 0;
+
+    for line in input.as_bytes().split(|at| *at == b'\n') {
+        let mut vs = [0, 0];
+        let mut state = 0;
+
+        for i in line.iter().copied() {
+            let (s, a) = unsafe {
+                *fwd.get_unchecked(state as usize * 256 + i as usize)
+            };
+            state = s;
+
+            //println!("{state} {:?} -> {s} {a}", i as char);
+
+            if a != 0 {
+                //println!("a: {a}");
+                vs[0] = a as i32;
+                break;
+            }
+        }
+
+        for i in line.iter().rev().copied() {
+            let (s, a) = unsafe {
+                *bwd.get_unchecked(state as usize * 256 + i as usize)
+            };
+            state = s;
+
+            //println!("{state} {:?} -> {s} {a}", i as char);
+
+            if a != 0 {
+                //println!("a: {a}");
+                vs[1] = a as i32;
+                break;
+            }
+        }
+
+        result += vs[0]*10 + vs[1];
+    }
+
+    return result;
+}
+
+fn part_2_fsm2_threaded(input: &str, fwd: &[(u8, u8)], bwd: &[(u8, u8)]) -> i32 {
+    let n = if input.len() < 128 { 1 } else { 12 };
+
+    (0..n)
+    .into_par_iter()
+    .map(move |i| {
+        let chunk_size = input.len() / n;
+
+        let mut begin = chunk_size * i;
+        let mut end = chunk_size * (i+1);
+
+        while begin > 0 && input.as_bytes()[begin - 1] != b'\n' {
+            begin -= 1;
+        }
+
+        if i < n-1 {
+            while end > begin && input.as_bytes()[end - 1] != b'\n' {
+                end -= 1;
+            }
+        }
+        else {
+            end = input.len();
+        }
+
+        //println!("{begin}..{end}");
+
+        part_2_fsm2(&input[begin..end], fwd, bwd)
+    }).sum()
+}
+
+
+fn part_2_fsm2_vect(input: &str, fwd: &[(u8, u8)], bwd: &[(u8, u8)]) -> i32 {
+    let mut result = 0;
+
+    let mut input = input.as_bytes();
+    while input.len() > 0 {
+        let line = {
+            let mut len = 0;
+
+            while len + 8 < input.len() {
+                let bytes = unsafe { input.as_ptr().add(len).cast::<u64>().read_unaligned() };
+                let bytes = bytes ^ 0x0a0a0a0a0a0a0a0a;
+                let zero_or_high = bytes.wrapping_sub(0x0101010101010101);
+                let not_high = !bytes & 0x8080808080808080;
+                let mask = zero_or_high & not_high;
+
+                if mask != 0 {
+                    len += (mask.trailing_zeros() / 8) as usize;
+                    break;
+                }
+                len += 8;
+            }
+
+            while len < input.len() && input[len] != b'\n' {
+                len += 1;
+            }
+
+            let line = &input[..len];
+            input = &input[len+1..];
+            line
+        };
+
+        let mut vs = [0, 0];
+        let mut state = 0;
+
+        for i in line.iter().copied() {
+            let (s, a) = unsafe {
+                *fwd.get_unchecked(state as usize * 256 + i as usize)
+            };
+            state = s;
+
+            //println!("{state} {:?} -> {s} {a}", i as char);
+
+            if a != 0 {
+                //println!("a: {a}");
+                vs[0] = a as i32;
+                break;
+            }
+        }
+
+        for i in line.iter().rev().copied() {
+            let (s, a) = unsafe {
+                *bwd.get_unchecked(state as usize * 256 + i as usize)
+            };
+            state = s;
+
+            //println!("{state} {:?} -> {s} {a}", i as char);
+
+            if a != 0 {
+                //println!("a: {a}");
+                vs[1] = a as i32;
+                break;
+            }
+        }
+
+        result += vs[0]*10 + vs[1];
+    }
+
+    return result;
+}
+
+fn part_2_fsm2_vect_threaded(input: &str, fwd: &[(u8, u8)], bwd: &[(u8, u8)]) -> i32 {
+    let n = if input.len() < 128 { 1 } else { 12 };
+
+    (0..n)
+    .into_par_iter()
+    .map(move |i| {
+        let chunk_size = input.len() / n;
+
+        let mut begin = chunk_size * i;
+        let mut end = chunk_size * (i+1);
+
+        while begin > 0 && input.as_bytes()[begin - 1] != b'\n' {
+            begin -= 1;
+        }
+
+        if i < n-1 {
+            while end > begin && input.as_bytes()[end - 1] != b'\n' {
+                end -= 1;
+            }
+        }
+        else {
+            end = input.len();
+        }
+
+        //println!("{begin}..{end}");
+
+        part_2_fsm2_vect(&input[begin..end], fwd, bwd)
+    }).sum()
+}
+
+
 fn run(f: impl FnOnce(&str) -> i32, input: &str) {
     let t0 = std::time::Instant::now();
     let result = f(input);
@@ -274,7 +462,9 @@ fn bench(f: impl Fn(&str) -> i32, n: u32, input: &str) {
 
 pub fn main() {
     for _ in 0..1000 {
-        if 0==1 { break }
+        #[cfg(debug_assertions)]
+        if 1==1 { break }
+
         (0..12).into_par_iter().for_each(|_| {
             part_1(include_str!("d01-prod.txt"));
             part_2(include_str!("d01-prod.txt"));
@@ -291,17 +481,40 @@ pub fn main() {
     run(part_2, include_str!("d01-prod.txt"));
     println!();
 
-    println!("part 2 fsm");
     let fsm = part_2_make_fsm();
+
+    println!("part 2 fsm");
     run(|i| part_2_fsm(i, &fsm), include_str!("d01-test-2.txt"));
     run(|i| part_2_fsm(i, &fsm), include_str!("d01-prod.txt"));
     println!();
 
     println!("part 2 fsm threaded");
-    let fsm = part_2_make_fsm();
     run(|i| part_2_fsm_threaded(i, &fsm), include_str!("d01-test-2.txt"));
     run(|i| part_2_fsm_threaded(i, &fsm), include_str!("d01-prod.txt"));
     println!();
+
+    let (fsm2a, fsm2b) = part_2_make_fsm2();
+
+    println!("part 2 fsm2");
+    run(|i| part_2_fsm2(i, &fsm2a, &fsm2b), include_str!("d01-test-2.txt"));
+    run(|i| part_2_fsm2(i, &fsm2a, &fsm2b), include_str!("d01-prod.txt"));
+    println!();
+
+    println!("part 2 fsm2 threaded");
+    run(|i| part_2_fsm2_threaded(i, &fsm2a, &fsm2b), include_str!("d01-test-2.txt"));
+    run(|i| part_2_fsm2_threaded(i, &fsm2a, &fsm2b), include_str!("d01-prod.txt"));
+    println!();
+
+    println!("part 2 fsm2 vect");
+    run(|i| part_2_fsm2_vect(i, &fsm2a, &fsm2b), include_str!("d01-test-2.txt"));
+    run(|i| part_2_fsm2_vect(i, &fsm2a, &fsm2b), include_str!("d01-prod.txt"));
+    println!();
+
+    println!("part 2 fsm2 vect threaded");
+    run(|i| part_2_fsm2_vect_threaded(i, &fsm2a, &fsm2b), include_str!("d01-test-2.txt"));
+    run(|i| part_2_fsm2_vect_threaded(i, &fsm2a, &fsm2b), include_str!("d01-prod.txt"));
+    println!();
+
 
     let mut big = String::new();
     for _ in 0..1000 {
@@ -309,18 +522,30 @@ pub fn main() {
     }
     run(|i| part_2_fsm(i, &fsm), &big);
     run(|i| part_2_fsm_threaded(i, &fsm), &big);
+    run(|i| part_2_fsm2(i, &fsm2a, &fsm2b), &big);
+    run(|i| part_2_fsm2_threaded(i, &fsm2a, &fsm2b), &big);
+    run(|i| part_2_fsm2_vect(i, &fsm2a, &fsm2b), &big);
+    run(|i| part_2_fsm2_vect_threaded(i, &fsm2a, &fsm2b), &big);
     println!();
 
     println!("bench part 2 10_000 iters");
     bench(|i| part_2(i), 10_000, include_str!("d01-prod.txt"));
     bench(|i| part_2_fsm(i, &fsm), 10_000, include_str!("d01-prod.txt"));
     bench(|i| part_2_fsm_threaded(i, &fsm), 10_000, include_str!("d01-prod.txt"));
+    bench(|i| part_2_fsm2(i, &fsm2a, &fsm2b), 10_000, include_str!("d01-prod.txt"));
+    bench(|i| part_2_fsm2_threaded(i, &fsm2a, &fsm2b), 10_000, include_str!("d01-prod.txt"));
+    bench(|i| part_2_fsm2_vect(i, &fsm2a, &fsm2b), 10_000, include_str!("d01-prod.txt"));
+    bench(|i| part_2_fsm2_vect_threaded(i, &fsm2a, &fsm2b), 10_000, include_str!("d01-prod.txt"));
     println!();
 
     println!("bench part 2 1000x cat'd, 100 iters");
     bench(|i| part_2(i), 100, &big);
     bench(|i| part_2_fsm(i, &fsm), 100, &big);
     bench(|i| part_2_fsm_threaded(i, &fsm), 100, &big);
+    bench(|i| part_2_fsm2(i, &fsm2a, &fsm2b), 100, &big);
+    bench(|i| part_2_fsm2_threaded(i, &fsm2a, &fsm2b), 100, &big);
+    bench(|i| part_2_fsm2_vect(i, &fsm2a, &fsm2b), 100, &big);
+    bench(|i| part_2_fsm2_vect_threaded(i, &fsm2a, &fsm2b), 100, &big);
     println!();
 }
 
