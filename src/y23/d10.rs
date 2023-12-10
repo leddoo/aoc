082434d16c66a444b3cc46a88e5bf4db19f9ead5
leddoo <leo@leddoo.net>
2023-12-10 13:37:47 +0100

    not actually faster

--- src/y23/d10.rs
+++ src/y23/d10.rs
@@ -1,9 +1,31 @@
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Step {
-    Up, Left, Down, Right
+    Up    = 1 << 0,
+    Left  = 1 << 1,
+    Down  = 1 << 2,
+    Right = 1 << 3,
 }
 
 impl Step {
+    #[inline]
+    fn options(at: u8) -> u32 {
+        match at {
+            b'S' => 0b1111,
+            b'|' => 0b0101,
+            b'-' => 0b1010,
+            b'L' => 0b1001,
+            b'J' => 0b0011,
+            b'7' => 0b0110,
+            b'F' => 0b1100,
+            _ => 0,
+        }
+    }
+
+    #[inline]
+    fn is_in(self, mask: u32) -> bool {
+        mask & (self as u32) != 0
+    }
+
     fn apply(self, x: &mut usize, y: &mut usize) {
         match self {
             Step::Up    => *y -= 1,
@@ -142,6 +164,15 @@ impl<'a> Grid<'a> {
     }
 }
 
+impl<'a> core::ops::Index<(usize, usize)> for Grid<'a> {
+    type Output = u8;
+
+    #[inline(always)]
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        &self.data[y*self.s + x]
+    }
+}
+
 fn part_1(input: &str) -> u64 {
     let (grid, (start_x, start_y)) = Grid::new(input);
 
@@ -254,6 +285,111 @@ fn part_2(input: &str) -> u64 {
 }
 
 
+// not sure why this isn't faster.
+fn part_2_fast(input: &str) -> u64 {
+    let (grid, (start_x, start_y)) = Grid::new(input);
+
+    let mut windings = vec![0i8; grid.s*grid.h];
+
+    let mut x = start_x;
+    let mut y = start_y;
+    let mut at_options = Step::options(b'S');
+    let mut len = 0;
+    let mut prev = 0;
+    while at_options != Step::options(b'S') || len == 0 {
+        // up.
+        if Step::Up.is_in(at_options)
+        && prev != Step::Down as u32
+        && y > 0 {
+            let to = grid[(x, y - 1)];
+
+            let to_options = Step::options(to);
+            if Step::Down.is_in(to_options) {
+                windings[y*grid.s + x] -= 1;
+                y -= 1;
+                windings[y*grid.s + x] -= 1;
+
+                at_options = to_options;
+                len += 1;
+                prev = Step::Up as u32;
+                continue
+            }
+        }
+
+        // left.
+        if Step::Left.is_in(at_options)
+        && prev != Step::Right as u32
+        && x > 0 {
+            let to = grid[(x - 1, y)];
+
+            let to_options = Step::options(to);
+            if Step::Right.is_in(to_options) {
+                x -= 1;
+
+                at_options = to_options;
+                len += 1;
+                prev = Step::Left as u32;
+                continue
+            }
+        }
+
+        // down.
+        if Step::Down.is_in(at_options)
+        && prev != Step::Up as u32
+        && y < grid.h-1 {
+            let to = grid[(x, y + 1)];
+
+            let to_options = Step::options(to);
+            if Step::Up.is_in(to_options) {
+                windings[y*grid.s + x] += 1;
+                y += 1;
+                windings[y*grid.s + x] += 1;
+
+                at_options = to_options;
+                len += 1;
+                prev = Step::Down as u32;
+                continue
+            }
+        }
+
+        // right.
+        if Step::Right.is_in(at_options)
+        && prev != Step::Left as u32
+        && x < grid.w-1 {
+            let to = grid[(x + 1, y)];
+
+            let to_options = Step::options(to);
+            if Step::Left.is_in(to_options) {
+                x += 1;
+
+                at_options = to_options;
+                len += 1;
+                prev = Step::Right as u32;
+                continue
+            }
+        }
+
+        break;
+    }
+
+    let mut inside = 0;
+    for y in 0..grid.h {
+        let mut was_inside = false;
+        let mut w = 0i8;
+        for x in 0..grid.w {
+            w += windings[y*grid.s + x];
+
+            let is_inside = w.abs() >= 2 && w % 2 == 0;
+            inside += (is_inside & was_inside) as u64;
+
+            was_inside = is_inside;
+        }
+        assert_eq!(w, 0);
+    }
+    return inside;
+}
+
+
 fn run(name: &str, f: impl FnOnce(&str) -> u64, input: &str) {
     let t0 = std::time::Instant::now();
     let result = f(input);
@@ -272,6 +408,11 @@ pub fn main() {
     run("part_2", part_2, include_str!("d10-test-2.txt"));
     run("part_2", part_2, include_str!("d10-test-3.txt"));
     run("part_2", part_2, include_str!("d10-prod.txt"));
+
+    run("part_2_fast", part_2_fast, include_str!("d10-test.txt"));
+    run("part_2_fast", part_2_fast, include_str!("d10-test-2.txt"));
+    run("part_2_fast", part_2_fast, include_str!("d10-test-3.txt"));
+    run("part_2_fast", part_2_fast, include_str!("d10-prod.txt"));
 
     println!();
 }
